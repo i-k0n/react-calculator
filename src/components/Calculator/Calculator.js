23ae611a584c235977ee2a49667091dbd72b8da1
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-03-26 22:26:42 -0500

    logic in place

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -7,10 +7,38 @@ function Calculator() {
     const [equation, setEquation] = useState('');
     const [result, setResult] = useState(0);
 
+    const onButtonPress = e => {
+      let equationTemp = equation;
+      const pressedButton = e.target.innerHTML;
+      if (pressedButton === 'C') return clear();
+      else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equationTemp += pressedButton;
+      else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equationTemp += ' ' + pressedButton + ' ';
+      else if (pressedButton === '=') {
+        try {
+          const evalResult = eval(equationTemp);
+          const result = Number.isInteger(evalResult)? evalResult : evalResult.toFixed(2);
+          setResult(result);
+        } catch (error) {
+          alert('Invalid Mathematical Equation');
+        }
+      }
+      else {
+        equationTemp = equationTemp.trim();
+        equationTemp = equationTemp.substr(0, equationTemp.length - 1);
+      }
+                  
+      setEquation(equationTemp);
+    }
+
+    const clear = () => {
+      setEquation('');
+      setResult(0);
+    }
+
     return (
       <main className="calculator">
         <Screen equation={equation} result={result} />
-        <Keypad />
+        <Keypad onButtonPress={onButtonPress} />
       </main>
     )
 }
