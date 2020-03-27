093fee733225bfbc30d46534cac9090370f23fcd
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-03-27 13:19:18 -0500

    refactoring math

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -8,26 +8,49 @@ function Calculator() {
     const [result, setResult] = useState(0);
 
     const onButtonPress = e => {
-      let equationTemp = equation;
-      const pressedButton = e.target.innerHTML;
-      if (pressedButton === 'C') return clear();
-      else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equationTemp += pressedButton;
-      else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equationTemp += ' ' + pressedButton + ' ';
-      else if (pressedButton === '=') {
-        try {
-          const evalResult = eval(equationTemp);
-          const result = Number.isInteger(evalResult)? evalResult : evalResult.toFixed(2);
-          setResult(result);
-        } catch (error) {
-          alert('Invalid Mathematical Equation');
-        }
+      // let equationTemp = equation;
+      // const pressedButton = e.target.innerHTML;
+      const key = e.target.innerHTML;
+      const action = e.target.getAttribute('data-action');
+      console.log(e.target.innerHTML)
+      console.log(e.target.getAttribute('data-action'))
+
+      if (!action) {
+        console.log('number key!')
       }
-      else {
-        equationTemp = equationTemp.trim();
-        equationTemp = equationTemp.substr(0, equationTemp.length - 1);
+
+      if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide' ||
+        action === 'percentage'
+        ) {
+          console.log('operator key!')
       }
+      // handle clear
+      // if (pressedButton === 'C') return clear();
+      // // handle numbers
+      // else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equationTemp += pressedButton;
+      // // handle operations
+      // else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equationTemp += ' ' + pressedButton + ' ';
+      // // handle equals
+      // else if (pressedButton === '=') {
+      //   try {
+      //     const evalResult = eval(equationTemp);
+      //     const result = Number.isInteger(evalResult)? evalResult : evalResult.toFixed(2);
+      //     setResult(result);
+      //   } catch (error) {
+      //     alert('Invalid Mathematical Equation');
+      //   }
+      // }
+      // // handle delete
+      // else {
+      //   equationTemp = equationTemp.trim();
+      //   equationTemp = equationTemp.substr(0, equationTemp.length - 1);
+      // }
                   
-      setEquation(equationTemp);
+      // setEquation(equationTemp);
     }
 
     const clear = () => {
