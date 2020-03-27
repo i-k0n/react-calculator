b7de929611b3b18a16c0cc8020123281b2e75df9
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-03-26 19:59:36 -0500

    created calculator component file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
+
+// click a number
+// clicking another number adds to that number
+// if an operator is selected, switch to second number
+// keep adding to second number
+// if equals is pressed, check operator
+// then perform math and display result
+
+
 
 function App() {
 
-  const [display, setDisplay] = useState("");
+  
 
   return (
     <div className="App">
-      <div className="calculator-container">
-        <div className="display">
-          {display}
-        </div>
-        <div className="buttons-container">
-          <button className="btn btn-clear">AC</button>
-          <button className="btn btn-plus-minus">&#177;</button>
-          <button className="btn btn-percent">&#37;</button>
-          <button className="btn btn-divide">&divide;</button>
-          <button className="btn btn-number" value="7" onClick={e => setDisplay(e.target.value)}>7</button>
-          <button className="btn btn-number" value="8" onClick={e => setDisplay(e.target.value)}>8</button>
-          <button className="btn btn-number" value="9" onClick={e => setDisplay(e.target.value)}>9</button>
-          <button className="btn btn-multiply">&times;</button>
-          <button className="btn btn-number" value="4" onClick={e => setDisplay(e.target.value)}>4</button>
-          <button className="btn btn-number" value="5" onClick={e => setDisplay(e.target.value)}>5</button>
-          <button className="btn btn-number" value="6" onClick={e => setDisplay(e.target.value)}>6</button>
-          <button className="btn btn-subtract">&minus;</button>
-          <button className="btn btn-number" value="1" onClick={e => setDisplay(e.target.value)}>1</button>
-          <button className="btn btn-number" value="2" onClick={e => setDisplay(e.target.value)}>2</button>
-          <button className="btn btn-number" value="3" onClick={e => setDisplay(e.target.value)}>3</button>
-          <button className="btn btn-add">&#43;</button>
-          <button className="btn btn-number" value="0" onClick={e => setDisplay(e.target.value)}>0</button>
-          <button className="btn btn-decimal">&#46;</button>
-          <button className="btn btn-equal">&#61;</button>
-        </div>
-      </div>
+      <Calculator />
     </div>
   );
 }
