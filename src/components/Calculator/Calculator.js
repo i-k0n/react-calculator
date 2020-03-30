558cf7ab611d78319243e77b903eacaf7aafbc13
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-03-30 12:46:11 -0500

    reworking logic

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,98 +6,162 @@ function Calculator() {
 
     const [equation, setEquation] = useState('');
     const [result, setResult] = useState('0');
-    const [display, setDisplay] = useState('0');
+    // const [display, setDisplay] = useState('0');
+    const [firstValue, setFirstValue] = useState('')
+    const [operator, setOperator] = useState('')
+    const [secondValue, setSecondValue] = useState('')
+    const [previousKey, setPreviousKey] = useState('')
 
     const onButtonPress = e => {
-      // let equationTemp = equation;
-      // const pressedButton = e.target.innerHTML;
+      let equationTemp = equation;
+      const pressedButton = e.target.innerHTML;
       const key = e.target;
       const action = e.target.getAttribute('data-action');
       const keyContent = key.textContent;
-      const displayedNum = display.textContent;
+      // const displayedNum = display.textContent;
       const calculator = document.querySelector('.calculator')
-      const previousKeyType = calculator.dataset.previousKeyType;
+      // const previousKeyType = calculator.dataset.previousKeyType;
 
-      console.log(keyContent)
-      console.log(e.target.getAttribute('data-action'))
-      if (!action) {
-        console.log('number key!')
-        calculator.dataset.previousKeyType = 'number'
-        if (result === '0' || previousKeyType === 'operator') {
-          setResult(keyContent);
-        } else { 
-          setResult(result + keyContent);
-        }
-      }
-
-      document.querySelector('.keypad').querySelectorAll('.btn').forEach(k => k.classList.remove('is-depressed'))
-      
-
-      
-
-      if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide' ||
-        action === 'percentage'
-        ) {
-          console.log('operator key!')
-          
-          key.classList.add('is-depressed')
-
-          // Add custom attribute
-          // make previousKeyType into state
-          calculator.dataset.previousKeyType = 'operator'
-          calculator.dataset.firstValue = result;
-          calculator.dataset.operator = action;
-      }
-
-      if (action === 'decimal') {
-        console.log('decimal key!')
-        calculator.dataset.previousKeyType = 'decimal'
-        if (!result.includes('.')) {
-          setResult(result + '.')
-        }
-      }
-
-      if (action === 'clear') {
-        console.log('clear key!')
-        calculator.dataset.previousKeyType = 'clear'
-      }
-
-      if (action === 'calculate') {
-        console.log('equal key!')
-        calculator.dataset.previousKeyType = 'calculate'
-        const firstValue = calculator.dataset.firstValue;
-        const operator = calculator.dataset.operator;
-        const secondValue = result;
-
-        setResult(calculate(firstValue, operator, secondValue))
-      }
-      // handle clear
-      // if (pressedButton === 'C') return clear();
-      // // handle numbers
-      // else if ((pressedButton >= '0' && pressedButton <= '9') || pressedButton === '.') equationTemp += pressedButton;
-      // // handle operations
-      // else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equationTemp += ' ' + pressedButton + ' ';
-      // // handle equals
-      // else if (pressedButton === '=') {
-      //   try {
-      //     const evalResult = eval(equationTemp);
-      //     const result = Number.isInteger(evalResult)? evalResult : evalResult.toFixed(2);
-      //     setResult(result);
-      //   } catch (error) {
-      //     alert('Invalid Mathematical Equation');
+      // console.log(keyContent)
+      // console.log(e.target.getAttribute('data-action'))
+      // if (!action) {
+      //   // console.log('number key!')
+        
+      //   if (result === '0' || previousKey === 'operator') {
+      //     setResult(keyContent);
+      //   } else { 
+      //     setResult(result + keyContent);
       //   }
+
+      //   setPreviousKey('number');
+      //   console.log("firstValue: ", firstValue, "operator: ", operator, "secondValue: ", secondValue, "previousKey: ", previousKey)
       // }
-      // // handle delete
-      // else {
-      //   equationTemp = equationTemp.trim();
-      //   equationTemp = equationTemp.substr(0, equationTemp.length - 1);
+
+      // document.querySelector('.keypad').querySelectorAll('.btn').forEach(k => k.classList.remove('is-depressed'))
+      
+
+      
+
+      // if (
+      //   action === 'add' ||
+      //   action === 'subtract' ||
+      //   action === 'multiply' ||
+      //   action === 'divide' ||
+      //   action === 'percentage'
+      //   ) {
+      //     // console.log('operator key!')
+          
+      //     // const firstValue = calculator.dataset.firstValue;
+      //     // console.log('firstValue: ', firstValue);
+      //     // const operator = calculator.dataset.operator;
+      //     // console.log('operator: ', operator);
+      //     // setSecondValue(result);
+      //     // console.log('secondValue: ', secondValue);
+
+      //     if (firstValue && operator && previousKey !== 'operator') {
+      //       const calcValue = calculate(firstValue, operator, secondValue)
+      //       console.log('calcValue: ', calcValue);
+      //       setResult(calcValue)
+
+      //       // update calculated value as firstValue
+      //       setFirstValue(calcValue)
+      //     } else if (firstValue) {
+      //       // if there are no calculations, set result as the firstValue
+      //       setResult(firstValue);
+      //     }
+
+      //     key.classList.add('is-depressed')
+
+      //     // Add custom attribute
+      //     setPreviousKey('operator');
+      //     setFirstValue(result)
+      //     setOperator(action);
+      //     console.log("firstValue: ", firstValue, "operator: ", operator, "secondValue: ", secondValue, "previousKey: ", previousKey)
       // }
+
+      // if (action === 'decimal') {
+      //   console.log('decimal key!')
+        
+      //   if (!result.includes('.')) {
+      //     setResult(result + '.')
+      //   } else if (previousKey === 'operator') {
+      //     setResult('0.')
+      //   }
+
+      //   setPreviousKey('decimal');
+      //   console.log("firstValue: ", firstValue, "operator: ", operator, "secondValue: ", secondValue, "previousKey: ", previousKey)
+      // }
+
+      // if (action === 'clear') {
+      //   console.log('clear key!')
+      //   return clear()
+      // }
+
+      // if (action === 'calculate') {
+      //   console.log('equal key!')
+      //   // let firstValue = calculator.dataset.firstValue;
+      //   // const operator = calculator.dataset.operator;
+      //   console.log(result)
+      //   setSecondValue(result);
+      //   console.log(secondValue)
+
+      //   if (firstValue) {
+      //     if (previousKey === 'calculate') {
+      //       setResult(firstValue)
+      //       setSecondValue(calculator.dataset.modValue);
+      //     }
+      //     setResult(calculate(firstValue, operator, secondValue))
+      //   }
+      //   calculator.dataset.modValue = secondValue;
+      //   setPreviousKey('calculate');
+      //   console.log("firstValue: ", firstValue, "operator: ", operator, "secondValue: ", secondValue, "previousKey: ", previousKey)
+      // }
+
+      // handle clear
+      if (pressedButton === 'C') {
+        return clear();
+      }
+      // handle numbers
+      else if (!action) {
+        equationTemp += pressedButton;
+      }
+      // handle decimals
+      else if (action === 'decimal') {
+        // check if last key was a decimal
+        const isDecimal = equationTemp.charAt(equationTemp.length - 1) === '.';
+        // console.log(isDecimal)
+        if (!isDecimal) {
+          if (equationTemp === '') {
+            equationTemp += '0.';  
+          } else {
+            equationTemp += '.';
+          }
+        } else if (previousKey === 'operator') {
+          equationTemp += '0.';
+        }
+    
+        //     setPreviousKey('decimal');
+        //     console.log("firstValue: ", firstValue, "operator: ", operator, "secondValue: ", secondValue, "previousKey: ", previousKey)
+      }
+      // handle operations
+      else if (['+', '-', '*', '/', '%'].indexOf(pressedButton) !== -1) equationTemp += ' ' + pressedButton + ' ';
+      // handle equals
+      else if (pressedButton === '=') {
+        try {
+          const evalResult = eval(equationTemp);
+          const result = Number.isInteger(evalResult)? evalResult : evalResult.toFixed(2);
+          setResult(result);
+        } catch (error) {
+          alert('Invalid Mathematical Equation');
+        }
+      }
+      // handle delete
+      else {
+        equationTemp = equationTemp.trim();
+        equationTemp = equationTemp.substr(0, equationTemp.length - 1);
+      }
                   
-      // setEquation(equationTemp);
+      setEquation(equationTemp);
     }
 
     const calculate = (n1, operator, n2) => {
@@ -114,6 +178,16 @@ function Calculator() {
       }
 
       return result
+    }
+
+    const clear = () => {
+      setResult('0')
+      setFirstValue('')
+      setOperator('')
+      setSecondValue('')
+      setPreviousKey('clear');
+      setEquation('')
+      console.log("firstValue: ", firstValue, "operator: ", operator, "secondValue: ", secondValue, "previousKey: ", previousKey)
     }
 
     return (
