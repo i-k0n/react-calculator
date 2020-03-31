d7eb81b3973d9c08806804fb35359ebaed839f51
i-k0n <19754246+i-k0n@users.noreply.github.com>
2020-03-31 15:06:02 -0500

    Updates

---
+++ precache-manifest.620ad3a715aeecf0ecf882288d7e2fbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986adf0d514fa653d9339870e9d1789c",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "f01fefc2da191474e746",
+    "url": "/react-calculator/static/css/main.1c3db866.chunk.css"
+  },
+  {
+    "revision": "d2e28318d8248ab72ae8",
+    "url": "/react-calculator/static/js/2.ced2ee69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.ced2ee69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01fefc2da191474e746",
+    "url": "/react-calculator/static/js/main.9abfd9d1.chunk.js"
+  },
+  {
+    "revision": "1efbf9ef7b1c2f1ed578",
+    "url": "/react-calculator/static/js/runtime-main.12eaf142.js"
+  }
+]);
